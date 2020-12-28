bb527679e3a607181475721a013b60b63fc5f163
austinare <austinjrichards@outlook.com>
2020-12-28 11:05:21 -0700

    added criteria for while loop, still having issues with the loop not exiting when conditions are met

--- binary-search.py
+++ binary-search.py
@@ -9,37 +9,39 @@ def search_numb(search_val, vec):
         vec: a vector of numbers in ascending order
         search_val: an integer or float 
     outputs:
-        loc: location of the value
+        center: location of the value
     '''
-    n          = len(vec)
-    center     = int(np.floor( n / 2 ))
-    center_val = vec[center]
+    center_val = 0
+    n = float('Inf')
 
-    while center_val != search_val:
+    while center_val != search_val or n > 1:
         n          = len(vec)
         center     = int(np.floor(n/2))
-        center_val = vec[center]
+        center_val = int(vec[center])
+        print(center_val)
+        print(search_val)
 
         if search_val < center_val:
             vec = vec[0:center]
         else:
             vec = vec[center:n]
+        print(vec)
     
-    return center
+    if center_val == search_val:
+        return center
+    else:
+        return 'value not in list'
 
 
 # generate the (sorted) list
-n   = 50
+n   = 10
 lis = np.zeros(n)
 
 for i in range(0,n):
-    lis[i] = (i+1)*2  # every other value up to 2n starting with 2
+    lis[i] = ( i + 1 ) * 2  # every other value up to 2n starting with 2
 
-print('Enter a value to search from 0 to', n)
-#search_val = int(input())  # take an input from the user, put it into search value
-search_val = 6
+search_val = 16  # search value
 
 # call the function
 a = search_numb(search_val, lis)
 print(a)
-print(lis)
